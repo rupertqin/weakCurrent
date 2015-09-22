7706cb28cb79eb6097e509fb29e7013a17f0149e
Qin Xiang <rupert.qing@gmail.com>
2015-09-22 14:59:53 +0800

    init

---
+++ app/js/common.js
@@ -0,0 +1,11 @@
+let rupert = "xiang"
+let b = 22
+
+let c = rupert + b
+
+function function_name (argument) {
+	// body...
+	return rupert + b + c
+}
+
+document.body.innerHTML = c
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    // entry: "./public/js/common.js",
+    entry: {
+        app: ["./app/js/common.js"]
+    },
+    output: {
+        path: __dirname + '/public/build/js/',
+        filename: "[name].bundle.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.scss$/, loader: 'style!raw!autoprefixer?{browsers:["safari >= 7", "Firefox 15", "ie >= 8", "chrome >= 34"]}!sass' },
+            { test: /\.css$/, loader: 'style!raw' },
+            { test: /\.(png|gif|jpg)$/, loader: 'file?name=img/[name].[ext]&path=../../img/[name].[ext]' },
+            // { test: /\.png$/, loader: 'url?mimetype=image/png&limit=500' },
+            { test: /\.jsx?$/, exclude: /(node_modules|bower_components)/, loader: 'babel'}
+        ]
+    },
+    devServer: {
+        contentBase: "./public",
+        port: "8080",
+        host: "0.0.0.0"
+    }
+};
