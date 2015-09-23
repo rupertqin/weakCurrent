52a46a6df8e1b5e62b6bd81840a6460c661d355f
Rupert <rupert.qing@gmail.com>
2015-09-24 03:16:21 +0800

    rm babel

--- app/js/search.js
+++ app/js/search.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Router, Route, Link, Redirect } from 'react-router';
-import { Button } from 'react-bootstrap';
+import "../scss/common.scss";
 
 const buttonsInstance = (
-    <Button>Default</Button>
+    <div>
+        <button className="btn btn-small btn-primary" type="button">Small button</button>
+        <button className="btn btn-small" type="button">Small button</button>
+    </div>
 
 );
 var Buttons = React.createClass({
@@ -22,13 +25,9 @@ var Search = React.createClass({
         </ul>
         <Buttons />
         <img src="img/logo.jpg" />
-        <div className="control-group">
-            <div className="controls">
-                <div className="input-prepend">
-                    <span className="add-on"><i className="icon-search"></i></span>
-                    <input className="span2" type="text" />
-                </div>
-            </div>
+        <div className="input-prepend">
+            <span className="add-on"><i className="icon-search"></i></span>
+            <input className="span2" type="text" />
         </div>
         <div className="control-label">
             <small> Email address </small>
