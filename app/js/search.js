d60a247cc799fd7378a546b08e8125edade9fff9
Qin Xiang <rupert.qing@gmail.com>
2015-09-23 22:09:20 +0800

    add bootstrap react

--- app/js/search.js
+++ app/js/search.js
@@ -1,20 +1,39 @@
 import React from 'react';
 import { Router, Route, Link, Redirect } from 'react-router';
 
+// import { Button } from 'react-bootstrap';
+
+var reactBootstrap = require("react-bootstrap")
+
+var Button = reactBootstrap.Button
+
+
+
+const buttonsInstance = (
+    <Button>Default</Button>
+
+);
+var Buttons = React.createClass({
+    render() {
+        return  buttonsInstance
+    }
+})
+
 var Search = React.createClass({
   render() {
     return (
-      <div id="search">
+      <div className="page-search">
         <ul>
           <li><Link to="/user/123">Bob</Link></li>
           <li><Link to="/user/abc">Sally</Link></li>
         </ul>
+        <Buttons />
         <img src="img/logo.jpg" />
         <div className="control-group">
             <div className="controls">
                 <div className="input-prepend">
                     <span className="add-on"><i className="icon-search"></i></span>
-                    <input className="span2" id="inputIcon" type="text" />
+                    <input className="span2" type="text" />
                 </div>
             </div>
         </div>
