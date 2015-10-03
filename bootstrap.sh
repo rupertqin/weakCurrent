5abacebf4e4bb4d77c3e051b0dff1b25169e2c3a
Rupert <rupert.qing@gmail.com>
2015-10-03 17:35:58 +0800

    add vagrant

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,3 +6,9 @@ if ! [ -L /var/www ]; then
   rm -rf /var/www
   ln -fs /vagrant /var/www
 fi
+
+echo "--- installing nvm ---"
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.28.0/install.sh | bash
+
+nvm install v4.1.1
+nvm alias default v4.1.1
